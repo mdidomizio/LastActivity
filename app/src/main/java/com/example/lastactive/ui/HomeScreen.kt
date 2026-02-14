3889504becb0e561ede7dfba1cc1bb367cdc7282
mdidomizio <miriam.didomizio@googlemail.com>
2026-02-14 12:24:59 +0100

    creating UI

---
+++ app/src/main/java/com/example/lastactive/ui/HomeScreen.kt
@@ -0,0 +1,137 @@
+package com.example.lastactive.ui
+
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.lastactive.R
+import com.example.lastactive.ui.theme.Figtree
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    modifier: Modifier = Modifier
+){
+    val backgroundColor = colorResource(id = R.color.background)
+    val activityStatus = remember { "Active now" }
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                modifier = Modifier
+                    .padding(horizontal = 16.dp),
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.home_screen_title),
+                        fontFamily = Figtree,
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 16.sp,
+                        color = colorResource(id = R.color.text_primary)
+                    )
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = {},
+                        modifier = Modifier
+                            .border(
+                            color = colorResource(R.color.outline),
+                            width = 1.dp,
+                                shape = CircleShape
+                        )
+                            .size(44.dp),
+                        colors = IconButtonDefaults.iconButtonColors(
+                            containerColor = colorResource(id = R.color.background),
+                            contentColor = colorResource(id = R.color.text_primary)
+                        )
+                    ) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = stringResource(id = R.string.back_button_content_description),
+                            modifier = Modifier.size(20.dp),
+                            tint = colorResource(id = R.color.text_secondary)
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = backgroundColor
+                )
+            )
+        },
+        containerColor = backgroundColor
+
+    ) { innerPadding ->
+        Column(
+            modifier = modifier
+                .padding(innerPadding)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(16.dp))
+            Box(
+                modifier = Modifier
+                    .size(140.dp)
+                    .background(
+                        color = colorResource(id = R.color.outline),
+                        shape = CircleShape
+                    ),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.avatar_img),
+                    modifier = Modifier
+                        .size(140.dp)
+                        .clip(CircleShape),
+                    contentDescription = stringResource(R.string.profile_image_content_description),
+                    contentScale = ContentScale.Crop
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = stringResource(id = R.string.profile_name),
+                fontFamily = Figtree,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 32.sp,
+                color = colorResource(id = R.color.text_primary)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = activityStatus,
+                fontFamily = Figtree,
+                fontWeight = FontWeight.Normal,
+                fontSize = 16.sp,
+                color = colorResource(id = R.color.text_secondary)
+            )
+        }
+
+
+    }
+}
